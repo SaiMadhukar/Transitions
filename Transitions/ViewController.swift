7e49ed5e6e4b5a6b9bf36166828a7ec06dd3f0cc
Sai Madhukar <saimadhukar@sais-macbook-air.local>
2017-07-27 15:00:31 +0530

    Initial Commit

---
+++ Transitions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Transitions
+//
+//  Created by Sai Madhukar on 27/07/17.
+//  Copyright © 2017 Sai Madhukar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
