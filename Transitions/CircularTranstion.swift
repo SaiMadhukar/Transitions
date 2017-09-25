4c12d1ca9f0806a3a61ee2ab6fea31d13de974a0
Sai Madhukar <saimadhukar@Sais-MacBook-Air.local>
2017-09-25 09:38:25 +0530

    Circular Transtions Working

---
+++ Transitions/CircularTranstion.swift
@@ -0,0 +1,13 @@
+//
+//  CircularTranstion.swift
+//  Transitions
+//
+//  Created by Sai Madhukar on 27/07/17.
+//  Copyright © 2017 Sai Madhukar. All rights reserved.
+//
+
+import UIKit
+
+class CircularTranstion: NSObject {
+
+}
---
+++ Transitions/SquareTransition.swift
@@ -0,0 +1,13 @@
+//
+//  SquareTransition.swift
+//  Transitions
+//
+//  Created by Sai Madhukar on 27/07/17.
+//  Copyright © 2017 Sai Madhukar. All rights reserved.
+//
+
+import UIKit
+
+class SquareTransition: NSObject {
+
+}
